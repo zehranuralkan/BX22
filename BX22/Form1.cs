ba45efedaf4c7756e7bad9682c30f8ff95d1f608
ZEHRA NUR ALKAN <68777717+zehranuralkan@users.noreply.github.com>
2021-08-09 15:58:21 +0300

    BX22

---
+++ BX22/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BX22
+{
+    public partial class Serial : Form
+    {
+        public Serial()
+        {
+            InitializeComponent();
+        }
+
+        public static SerialPort serialPort = new SerialPort();//serial port tanitimi
+        public static string[] ports = SerialPort.GetPortNames();
+
+        public void OpenConnection(string portName, int boudrate) {
+            if (serialPort.IsOpen)
+                serialPort.Close();
+            if (!serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.PortName = portName;
+                    serialPort.BaudRate = boudrate;
+                    serialPort.Open();
+                    serialPort.Handshake = Handshake.None; //baglanti noktasi iletisiimde denetim
+                    data_listbox.Items.Add("Serial port" + " " + cbx_name.Text +" "+ "is open");
+                    data_listbox.Items.Add(deger);
+                }
+                catch
+                {
+                    MessageBox.Show("Connection Error");
+                    data_listbox.Items.Add("Serial port" + " " + cbx_name.Text + " opening error");
+                }
+            }
+
+        }
+
+      
+        public string deger = "";
+        
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                deger = serialPort.ReadLine();
+                //this.Invoke(new EventHandler(displayData_event));
+                Control.CheckForIllegalCrossThreadCalls = false;
+                data_listbox.Items.Add(deger);
+                
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("Geçersiz i/o");
+            }
+            catch (InvalidOperationException)
+            {
+                deger = "";
+            }
+          
+            catch (FormatException)
+            {
+                MessageBox.Show("Geçersiz format");
+            }
+         
+            
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("Geçersiz dizin");
+
+            }
+
+        }
+
+        public void ComPortList()
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                cbx_name.Items.Add(port);
+            }
+           
+        }
+
+       
+     
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+           
+            if (cbx_name.Text!=" " && cbx_boud.Text!=" ")
+            {
+                OpenConnection(cbx_name.Text, Convert.ToInt32(cbx_boud.Text)); //kulanilacak port
+                //serialPort.PortName = cbx_name.SelectedItem.ToString();
+                backgroundWorker1.RunWorkerAsync();
+               
+
+            }
+           
+            else
+            {
+                MessageBox.Show("Gerekli alanları doldurunuz");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived); //verileri alır
+
+            ComPortList();
+        }
+    }
+}
